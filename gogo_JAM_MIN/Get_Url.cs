a8a19e4890799e727af79c1db94225fd09f17b2e
justkiwon <kkhiddd@gmail.com>
2022-11-06 20:26:18 +0900

    Parsing부터 하기

--- gogo_JAM_MIN/Get_Url.cs
+++ gogo_JAM_MIN/Get_Url.cs
@@ -22,7 +22,7 @@ namespace gogo_JAM_MIN
         int Success = 0;
         public string[,] Request(string url)
         {
-            string[,] Data = new string[4,4];
+            string[,] Data = new string[6,6];
 
             // HttpWebRequest 이용한 응답 수신 방법
             var request = (HttpWebRequest)WebRequest.Create(url);   // 요청 URL을 기준으로 HTTP요청 인스턴스 생성
@@ -51,13 +51,25 @@ namespace gogo_JAM_MIN
                     m++;
                     foreach (XmlNode node1 in xmlList)  // <item> 값 읽어 들이기
                     {
-                        if (node1["PRODNM"].InnerText != null) { node1["PRODNM"].InnerText = Data[m, n]; }
+                        if (node1["PRODNM"].InnerText == null) { continue; } // 유종 Data[m,0]
+                            Data[m, n++] = node1["PRODNM"].InnerText;
 
-                        if (node1["PRICE"].InnerText != null) { node1["PRICE"].InnerText = Data[m, n]; }
+                        if (node1["PRICE"].InnerText == null) { continue; }  // 가격 Data[m,1]
+                            Data[m, n++] = node1["PRICE"].InnerText;
 
-                        Data[m,n] = node1.InnerText;
-                        n++;
+                        if (node1["DIFF"].InnerText == null) { continue; }  // 전일과 가격 차이 비율 Data[m,1]
+                            Data[m, n++] = node1["DIFF"].InnerText;
+
+                        Data[m,n] = node1.InnerText;  // 20221106K015자동차용부탄1032.89-0.33
+                        n = 0;
+                        m++;
                     } n = 0;
+                    for(int i= 0; i< 6; i++){
+                        for (int j = 0; j < 6; j++)
+                        {
+                            Console.WriteLine("Data[{0}][{1}] = {2}", i, j, Data[i, j]);
+                        }
+                    }
                 }
                 else { MessageBox.Show("error"); }
                 m = 0;
